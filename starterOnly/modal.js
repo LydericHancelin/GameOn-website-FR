7bea26f35ff15278b2e7ac40f49116c6223a10c2
Lyderic <lyderic.hancelin@gmail.com>
2022-02-18 16:38:11 +0100

    :ok_hand: last changes for lisibility

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,6 +7,9 @@ function editNav() {
   }
 }
 
+// Contraints
+const $minInputLength = 2
+
 // DOM Elements
 const $modalbg = document.querySelector(".bground");
 const $modalhero = document.querySelector(".hero-section");
@@ -52,138 +55,85 @@ function handleSubmit(event) {
   event.preventDefault();
 }
 
-// launch modal event
-$modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-// close modal event
-$closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
-
-
-// listen submit event
-$subscribeForm.addEventListener("submit", handleSubmit);
-
-const form = document.getElementsByTagName('form')[0];
-
-// tests verifs mail
 const $inputEmail = document.getElementById('input-email');
-
-
-$inputEmail.addEventListener("input", function () {
-  if ($inputEmail.validity.valid) {
-    $formdataEmail.setAttribute("data-error-visible", false);
-  }
-  else {
-    $formdataEmail.setAttribute("data-error-visible", true);
-  }
-}, false);
-
-
-// tests verif FirstName
 const $inputFirstName = document.getElementById('input-firstname');
-
-$inputFirstName.addEventListener("input", function () {
-  if ($inputFirstName.value.length >= 2) {
-    $formdataFirstName.setAttribute("data-error-visible", false); // On réinitialise le contenu
-  }
-  else {
-    $formdataFirstName.setAttribute("data-error-visible", true);
-  }
-}, false);
-
-// tests verif LastName
 const $inputLastName = document.getElementById('input-lastname');
-
-$inputLastName.addEventListener("input", function () {
-  if ($inputLastName.value.length >= 2) {
-    $formdataLastName.setAttribute("data-error-visible", false); // On réinitialise le contenu
-  }
-  else {
-    $formdataLastName.setAttribute("data-error-visible", true);
-  }
-}, false);
+const $inputBirthDate = document.getElementById('input-birthdate');
+const $inputNbreTournament = document.getElementById('input-nbretournament');
 
 //fonctions utiles
 function isDateValid(date) {
   return date instanceof Date && !isNaN(date.getTime());
 }
-
-//tests verif date
-const $inputBirthDate = document.getElementById('input-birthdate');
-
-$inputBirthDate.addEventListener("input", function () {
-  const birthdate = new Date($inputBirthDate.value);
-
-  if (isDateValid(birthdate)) {
-    $formdataBirthDate.setAttribute("data-error-visible", false); // On réinitialise le contenu
-  }
-  else {
-    $formdataBirthDate.setAttribute("data-error-visible", true);
-  }
-}, false);
-
-
-//fonctions utiles
+const isEmailValid = email => email.validity.valid
 const isPositiveInteger = num => Number.isInteger(num) && num >= 0
 
-//tests verif tournois
-const $inputNbreTournament = document.getElementById('input-nbretournament');
-
-
-$inputNbreTournament.addEventListener("input",
-  e => $formdataNbreTournament.setAttribute("data-error-visible", !isPositiveInteger(parseInt(e.target.value,10))))
-
-
-//tests verif radio /!\ peut etre remplacé par un required
-
-
+//Check if only one radio is check
 const isChecked = ($formdataRadio) => Array.from($formdataRadio.children)
-  .filter(el=>el.type==="radio")
-  .reduce((acc,el) => el.checked ? ++acc : acc,0) === 1
+  .filter(el => el.type === "radio")
+  .reduce((acc, el) => el.checked ? ++acc : acc, 0) === 1
 
 const hasDataErrorVisible = domElement => domElement.getAttribute("data-error-visible") === "true"
 
+const isFormValid = () => {
+  return !hasDataErrorVisible($formdataRadio) && !hasDataErrorVisible($formdataCgu)
+    && !hasDataErrorVisible($formdataBirthDate) && !hasDataErrorVisible($formdataEmail)
+    && !hasDataErrorVisible($formdataFirstName) && !hasDataErrorVisible($formdataLastName)
+    && !hasDataErrorVisible($formdataNbreTournament)
+}
+
+//Event listeners
+
+// launch modal event
+$modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+$closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+// listen submit event
+$subscribeForm.addEventListener("submit", handleSubmit);
+
+$inputEmail.addEventListener("input", function () {
+  setErrorValidity($inputEmail, $formdataEmail, isEmailValid)
+}, false);
+
+$inputFirstName.addEventListener("input", function () {
+  $formdataFirstName.setAttribute('data-error-visible', $inputFirstName.value.length >= $minInputLength)
+}, false);
+
+$inputLastName.addEventListener("input", function () {
+  $formdataLastName.setAttribute('data-error-visible', $inputLastName.value.length >= $minInputLength)
+}, false);
+
+$inputBirthDate.addEventListener("input", function () {
+  const birthdate = new Date($inputBirthDate.value);
+  $formdataBirthDate.setAttribute('data-error-visible', !isDateValid(birthdate))
+}, false);
+
+$inputNbreTournament.addEventListener("input",
+  e => $formdataNbreTournament.setAttribute("data-error-visible", !isPositiveInteger(parseInt(e.target.value, 10))))
+
+
 //Vérifications au "submit"
-form.addEventListener("submit", function (event) {
+$form.addEventListener("submit", function (event) {
   event.preventDefault();
   $formdataRadio.setAttribute("data-error-visible", !isChecked($formdataRadio))
 
   const $inputCheckboxCgu = document.getElementById('input-checkboxcgu')
   $formdataCgu.setAttribute("data-error-visible", !$inputCheckboxCgu.checked)
 
-  if($inputFirstName.value ===""){
-    $formdataFirstName.setAttribute("data-error-visible", true);
+  $formdataFirstName.setAttribute('data-error-visibility', $inputFirstName.value === "")
+  $formdataLastName.setAttribute('data-error-visibility', $inputLastName.value === "")
+  $formdataEmail.setAttribute('data-error-visibility', $inputEmail.value === "")
+  if(isFormValid()){
+    $form.style.display = "none";
+    $modalThanks.style.display = "flex";
   }
-  else{
-    $formdataFirstName.setAttribute("data-error-visible", false);
-  }
-
-  if($inputLastName.value ===""){
-    $formdataLastName.setAttribute("data-error-visible", true);
-  }
-  else{
-    $formdataLastName.setAttribute("data-error-visible", false);
-  }
-  if($inputEmail.value ===""){
-    $formdataEmail.setAttribute("data-error-visible", true);
-  }
-  else{
-    $formdataEmail.setAttribute("data-error-visible", false);
-  }
-
-  if(!hasDataErrorVisible($formdataRadio) && !hasDataErrorVisible($formdataCgu)
-    && !hasDataErrorVisible($formdataBirthDate) && !hasDataErrorVisible($formdataEmail)
-    && !hasDataErrorVisible($formdataFirstName) && !hasDataErrorVisible($formdataLastName)
-    && !hasDataErrorVisible($formdataNbreTournament)){
-    $form.style.display="none";
-    $modalThanks.style.display="flex";
-  }
-
 }, false);
 
 $buttonThanks.addEventListener("click", () => {
-    $form.style= null ;
-    $modalThanks.style=null;
-    closeModal();
-    $form.reset();
+  $form.style = null;
+  $modalThanks.style = null;
+  closeModal();
+  $form.reset();
 })
 
 
